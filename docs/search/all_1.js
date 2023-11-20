3845b67cf29869c341652606496cba3ec028b0ac
Gust4vo73 <151526988+Gust4vo73@users.noreply.github.com>
2023-11-20 09:50:05 -0300

    Add files via upload

---
+++ docs/search/all_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['main_0',['main',['../ex_011_8c.html#ae66f6b31b5ad750f1fe042a706a4e3d4',1,'ex 1.c']]]
+];
---
+++ docs/search/searchdata.js
@@ -0,0 +1,24 @@
+var indexSectionsWithContent =
+{
+  0: "emt",
+  1: "e",
+  2: "m",
+  3: "t"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "files",
+  2: "functions",
+  3: "defines"
+};
+
+var indexSectionLabels =
+{
+  0: "Todos",
+  1: "Arquivos",
+  2: "Funções",
+  3: "Definições e Macros"
+};
+
